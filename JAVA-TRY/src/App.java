67bbf9cbe7d36bbfd8dded70255419ffb8bd012b
daniel konforty <danielkon42@gmail.com>
2023-07-21 11:09:53 +0300

    asdasd

---
+++ JAVA-TRY/src/App.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        ArrayList myList = new ArrayList();
+        int[] asd = new int[310];
+
+        myList.add("hi");
+        myList.add("ert");
+
+        myList.set(1, "daniel");
+
+        System.out.println(myList.get(1));
+    }
+}
